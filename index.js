a176df4df90950cda8c2949888e3e86bd888c42f
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 07:49:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // Write your code here!
 let element =document.querySelector('main');
+element.remove();
 header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
