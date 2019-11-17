9b7adcfcb6f95a059df71bef44a6860500c5cb72
nawal abdualghani <nawalabdualghani@gmail.com>
2019-11-17 08:03:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let element =document.querySelector('main');
 element.remove();
 let newHeader = document.createElement('h1#victory');
 
-let element = document.querySelector("h1#victory");
-element.innerHTML = 'Hello, DOM!'
+let newHeader = document.querySelector("h1#victory");
+newHeader.innerHTML = 'Hello, DOM!'
 header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
